96a05e02aab99bd1f71dd85a25c5c6f126c7fc96
Merle <changmerle@gmail.com>
2018-05-23 11:17:11 +0800

    Update PoseEstimation.py
    Add English notes

--- PoseEstimation.py
+++ PoseEstimation.py
@@ -31,6 +31,7 @@ def poseEstimation(img, pt2d):
 def PoseEstimation(pt2d, pt3d):
 # 参照论文Optimum Fiducials Under Weak Perspective Projection，使用弱透视投影
     # 减均值，排除t，便于求出R
+    # minus mean value of all point so that t=0, we can calculate pure R
     pt2dm = np.zeros(pt2d.shape)
     pt3dm = np.zeros(pt3d.shape)
     pt2dm[0, :] = pt2d[0, :]-np.mean(pt2d[0, :])
@@ -39,9 +40,11 @@ def PoseEstimation(pt2d, pt3d):
     pt3dm[1, :] = pt3d[1, :]-np.mean(pt3d[1, :])
     pt3dm[2, :] = pt3d[2, :]-np.mean(pt3d[2, :])
     # 最小二乘方法计算R
+    # Calculate R using least squares algorithm
     R1 = np.dot(np.dot(np.mat(np.dot(pt3dm, pt3dm.T)).I, pt3dm), pt2dm[0, :].T)
     R2 = np.dot(np.dot(np.mat(np.dot(pt3dm, pt3dm.T)).I, pt3dm), pt2dm[1, :].T)
     # 计算出f
+    # calculate alpha (is f in this code)
     f = (math.sqrt(R1[0, 0]**2+R1[0, 1]**2+R1[0, 2]**2)+math.sqrt(R2[0, 0]**2+R2[0, 1]**2+R2[0, 2]**2))/2
     R1 = R1/f
     R2 = R2/f
